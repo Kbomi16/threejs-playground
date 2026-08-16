31611a209bcecccbf74c44d6a1af976be67f661e
Kbomi16 <kimbomi2172@naver.com>
2026-08-16 19:34:50 +0900

    ✨ R3F로 Pointer 이벤트 다루기

--- project01/chapter-03-r3f/src/components/GLBModel.jsx
+++ project01/chapter-03-r3f/src/components/GLBModel.jsx
@@ -1,9 +1,11 @@
 import { useAnimations, useGLTF } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function GLBModel() {
-  const three = useThree()
+  const [currentAnimation, setCurrentAnimation] = useState('wave')
+
+  // const three = useThree()
   const { scene, animations } = useGLTF('/dancer.glb')
   const ref = useRef(null)
 
@@ -15,13 +17,34 @@ export default function GLBModel() {
         obj.castShadow = true
         obj.receiveShadow = true
       }
-
-      actions['wave'].play()
     })
   }, [scene, actions])
+
+  useEffect(() => {
+    actions[currentAnimation].fadeIn(0.5).play()
+
+    return () => {
+      actions[currentAnimation].fadeOut(0.5).stop()
+    }
+  }, [currentAnimation, actions])
 
   // state 값 = useThree 로 얻은 값과 같다?
   useFrame((state, delta) => {})
 
-  return <primitive ref={ref} scale={0.01} object={scene} position-y={0.8} />
+  return (
+    <primitive
+      ref={ref}
+      onPointerUp={() => console.log('포인터 업')}
+      onPointerDown={() => console.log('포인터 다운')}
+      onClick={(prev) => {
+        setCurrentAnimation((prev) => {
+          if (prev === 'wave') return 'windmill'
+          return 'wave'
+        })
+      }}
+      scale={0.01}
+      object={scene}
+      position-y={0.8}
+    />
+  )
 }
