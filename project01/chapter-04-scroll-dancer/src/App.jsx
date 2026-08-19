e51de86f1f9aa6802c29dbcb62901bb8d3483523
Kbomi16 <kimbomi2172@naver.com>
2026-08-19 21:03:14 +0900

    ✨ React 컴포넌트 및 캔버스 세팅하기

--- project01/chapter-04-scroll-dancer/src/App.jsx
+++ project01/chapter-04-scroll-dancer/src/App.jsx
@@ -1,5 +1,18 @@
+import MainCanvas from './components/MainCanvas'
+import styled from 'styled-components'
+
 function App() {
-  return <>스크롤 댄서 프로젝트</>
+  return (
+    <Wrapper>
+      <MainCanvas />
+    </Wrapper>
+  )
 }
 
 export default App
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`
---
+++ project01/chapter-04-scroll-dancer/src/components/MainCanvas.jsx
@@ -0,0 +1,25 @@
+import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three'
+import { Box, OrbitControls } from '@react-three/drei'
+
+export default function MainCanvas() {
+  const aspectRatio = window.innerWidth / window.innerHeight
+  return (
+    <Canvas
+      id="canvas"
+      gl={{ antialias: true }} // 가장자리 계단 현상 방지
+      shadows="soft"
+      camera={{
+        fov: 30,
+        aspect: aspectRatio,
+        near: 0.01,
+        far: 1000,
+        position: [0, 6, 12],
+      }}
+      scene={{ background: new THREE.Color('#000') }}
+    >
+      <Box material-color="red" />
+      <OrbitControls />
+    </Canvas>
+  )
+}
