1951ef9b5844629bb9cd563d1611511f6777ed63
Kbomi16 <kimbomi2172@naver.com>
2026-09-01 21:36:06 +0900

    ✨ 기본 scene 구성 (canvas, box)

--- project02/example01/src/App.js
+++ project02/example01/src/App.js
@@ -1,7 +1,34 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, useHelper } from '@react-three/drei'
+import { useRef } from 'react'
+import { DirectionalLightHelper } from 'three'
+
 function App() {
   return (
-    <div className="h1">Hello R3F</div>
-  );
+    <Canvas camera={{ position: [0, 1, 2] }}>
+      <color attach="background" args={['yellow']} />
+      <Box rotation-y={1} />
+      <Box position={[0, 0, -1.5]} rotation-y={1} />
+      <Light />
+    </Canvas>
+  )
 }
 
-export default App;
+export default App
+
+function Box({ ...props }) {
+  return (
+    <mesh {...props}>
+      <boxGeometry args={[1]} />
+      <meshStandardMaterial color={'hotpink'} wireframe />
+    </mesh>
+  )
+}
+
+function Light() {
+  const ref = useRef()
+
+  useHelper(ref, DirectionalLightHelper, 1, 'red')
+
+  return <directionalLight ref={ref} position={[1, 1, -1]} intensity={3} />
+}
