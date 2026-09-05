7c891b7e2d09702c58d0b9fbcfa808a23359aa56
Kbomi16 <kimbomi2172@naver.com>
2026-09-05 16:43:00 +0900

    ✨ 비동기 데이터 로딩 처리하기 (suspense)

--- project02/example02/src/pages/index.js
+++ project02/example02/src/pages/index.js
@@ -1,15 +1,30 @@
 import { Canvas } from '@react-three/fiber'
-import Scene from '../components/Scene'
 import Lights from '../components/Lights'
+import { lazy, Suspense } from 'react'
 
-function Home() {
+export default function Home() {
   return (
     <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
       <color attach="background" args={['rgb(67, 127, 240) 100%)']} />
-      <Lights />
-      <Scene />
+      <Suspense fallback={<Sphere />}>
+        <Lights />
+        <Scene />
+      </Suspense>
     </Canvas>
   )
 }
 
-export default Home
+function Sphere() {
+  return (
+    <mesh>
+      <sphereGeometry args={[1]} />
+      <meshBasicMaterial color="white" />
+    </mesh>
+  )
+}
+
+const Scene = lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import('../components/Scene')), 1000)
+  })
+})
