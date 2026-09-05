7867769b4ef4d2bc367e5d0ef0172d75dfa3977d
Kbomi16 <kimbomi2172@naver.com>
2026-09-05 16:30:22 +0900

    ✨ 그룹 오브젝트 해당 오브젝트만 기억하기 (useMemo)

--- project02/example02/src/components/Weather.js
+++ project02/example02/src/components/Weather.js
@@ -1,17 +1,23 @@
 import { useLoader } from '@react-three/fiber'
+import { useMemo } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const Weather = ({ position, weather }) => {
   const glb = useLoader(GLTFLoader, '/models/weather.glb')
   console.log(glb.nodes)
 
-  let weatherModel
+  // let weatherModel
 
-  if (glb.nodes[weather]) {
-    weatherModel = glb.nodes[weather].clone()
-  } else {
-    weatherModel = glb.nodes.cloud.clone()
-  }
+  // if (glb.nodes[weather]) {
+  //   weatherModel = glb.nodes[weather].clone()
+  // } else {
+  //   weatherModel = glb.nodes.cloud.clone()
+  // }
+
+  const weatherModel = useMemo(() => {
+    const cloneModel = glb.nodes[weather] || glb.nodes.cloud
+    return cloneModel.clone()
+  }, [weather, glb.nodes])
 
   return (
     <mesh position={position}>
