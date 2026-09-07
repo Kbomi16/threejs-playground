4656e0f49303e874d5dc8918381f0329cbc6bcc2
Kbomi16 <kimbomi2172@naver.com>
2026-09-07 22:16:55 +0900

    ✨ framer-motion-3D 라이브러리

--- project02/example02/src/pages/index.js
+++ project02/example02/src/pages/index.js
@@ -1,14 +1,16 @@
 import { Canvas } from '@react-three/fiber'
 import Lights from '../components/Lights'
 import { lazy, Suspense } from 'react'
+import { motion } from 'r3f-motion'
 
 export default function Home() {
   return (
     <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
       <color attach="background" args={['rgb(67, 127, 240) 100%)']} />
-      <Suspense fallback={<Sphere />}>
+      <Suspense fallback={'loading...'}>
         <Lights />
-        <Scene />
+        {/* <Scene /> */}
+        <FramerModel position={[0, 0, 0]} />
       </Suspense>
     </Canvas>
   )
@@ -28,3 +30,51 @@ const Scene = lazy(() => {
     setTimeout(() => resolve(import('../components/Scene')), 1000)
   })
 })
+
+const variants = {
+  initial: {
+    rotateX: Math.PI / 2,
+    rotateZ: 1,
+  },
+  animate1: {
+    rotateZ: [0, Math.PI],
+    transition: {
+      duration: 3,
+      repeat: Infinity,
+    },
+  },
+  animate2: {
+    rotateY: [Math.PI, 0],
+    transition: {
+      duration: 3,
+      repeat: Infinity,
+    },
+  },
+}
+
+const materialVariants = {
+  initial: { opacity: 0.2 },
+  animate: {
+    opacity: [1, 0.5, 1],
+    transition: { duration: 0.5, repeat: Infinity },
+  },
+}
+
+function FramerModel() {
+  return (
+    <motion.mesh
+      variants={variants}
+      initial="initial"
+      animate="animate1"
+    >
+      <cylinderGeometry args={[1, 1, 0.5, 8]} />
+      <motion.meshBasicMaterial
+        transparent
+        variants={materialVariants}
+        initial="initial"
+        animate="animate"
+        color="hotpink"
+      />
+    </motion.mesh>
+  )
+}
